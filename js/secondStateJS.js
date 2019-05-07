e13cae771c249fb92bb92b76c3dc73690c79126b
Timothy McCallum <tpmccallum@users.noreply.github.com>
2019-05-07 16:01:11 +1000

    Update URL to "Play"
    Now includes the query string "contract=0x...."
    
    https://cybermiles.github.io/smart_contracts/FairPlay/dapp/play.html?contract=" + value._source.contractAddress;
    Line 385

--- js/secondStateJS.js
+++ js/secondStateJS.js
@@ -382,7 +382,7 @@ function renderItems(_hits) {
             var play = jQuery('<dd/>', {
 
             });
-            var playUrl = "https://cybermiles.github.io/smart_contracts/FairPlay/dapp/play.html?" + value._source.contractAddress;
+            var playUrl = "https://cybermiles.github.io/smart_contracts/FairPlay/dapp/play.html?contract=" + value._source.contractAddress;
             var playButton = jQuery('<a/>', {
                 href: playUrl,
                 class: "btn btn-success",
