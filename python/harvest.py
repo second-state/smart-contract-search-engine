8b9e0698172c04033fceaa6ffa87a1177705139e
tpmccallum <mistermac2008@gmail.com>
2019-05-11 09:58:13 +1000

    adding changes

--- python/harvest.py
+++ python/harvest.py
@@ -7,16 +7,16 @@ class Harvest:
         self.scriptExecutionLocation = os.getcwd()
         print("Reading configuration file")
         config = configparser.ConfigParser(interpolation=configparser.ExtendedInterpolation())
-        config.read('config.ini')
-
+        config.read(os.path.join(self.scriptExecutionLocation, 'config.ini'))
         # ABI[s]
         self.abis = {}
-        for key in config['smartContractAbis']:
+        for key in config['abis']:
             stringKey = str(key)
-            self.urls[stringKey] = config['smartContractAbis'][key]
+            self.abis[stringKey] = config['abis'][key]
         print("\nABIs:")
-        for (ufaKey, ufaValue) in self.urls.items():
+        for (ufaKey, ufaValue) in self.abis.items():
             print(ufaKey + ": " + ufaValue)
+
 
 # Driver - Start
 harvester = Harvest()
