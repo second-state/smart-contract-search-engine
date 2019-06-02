4eb09439fa8674d292ad361a993bd1b9003746d4
tpmccallum <mistermac2008@gmail.com>
2019-06-02 14:28:49 +1000

    self

--- python/abi_byte_processor.py
+++ python/abi_byte_processor.py
@@ -433,7 +433,7 @@ class Harvest:
         
 
     def updateBytecodeAndVersion(self, _abiSha3, _contractAddress):
-        jsonAbi = fetchAbiUsingHash(_abiSha3)
+        jsonAbi = self.fetchAbiUsingHash(_abiSha3)
         contractInstance = self.web3.eth.contract(abi=jsonAbi, address=_contractAddress)
         print("UPDATING BYTECODE WITH A NEW CONTRACT INSTANCE")
 
