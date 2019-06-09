99c84498b3735496f571b509e3777fbc694154bd
tpmccallum <mistermac2008@gmail.com>
2019-06-10 08:25:08 +1000

    No hoisting, move ABIs up

--- python/utilities/manually_compare_erc20_abis.py
+++ python/utilities/manually_compare_erc20_abis.py
@@ -45,44 +45,6 @@ blockchainRpc = config['blockchain']['rpc']
 
 web3 = Web3(HTTPProvider(str(blockchainRpc)))
 
-def getHashesForTesting(_abiData):
-    #ABI Text
-    abiData = re.sub(r"[\n\t]*", "", json.dumps(json.loads(_abiData), sort_keys=True))
-    abiData = re.sub(r"[\s]+", " ", abiData)
-    #Abi Sha
-    officialAbiSha = web3.toHex(web3.sha3(text=abiData))
-    # Abi JSON
-    officialAbiJSON = json.loads(abiData)
-    # Abi Functions
-    keccakHashes = []
-    for item in officialAbiJSON:
-        if item['type'] == 'function':
-            if len(item['inputs']) == 0:
-                stringToHash = str(item['name'] + '()')
-                hashCreated = str(web3.toHex(web3.sha3(text=stringToHash)))[2:10]
-                keccakHashes.append(hashCreated)
-            else:
-                tempString = ""
-                tempString += item['name'] + '('
-                iterator = 1
-                for functionInput in item['inputs']:
-                    if iterator == len(item['inputs']):
-                        tempString += str(functionInput['type'] + ')')
-                    else:
-                        tempString += str(functionInput['type'] + ',')
-                        iterator += 1
-                stringToHash = tempString
-                hashCreated = str(web3.toHex(web3.sha3(text=stringToHash)))[2:10]
-                keccakHashes.append(hashCreated)
-    return keccakHashes
-
-officialHashes = getHashesForTesting(officialAbi)
-makerHashes = getHashesForTesting(makerAbi)
-print("\nKeccak Hashes for official ERC20 ABI:")
-print(officialHashes)
-print("\nKeccak Hashes for Maker (MKR) ABI:")
-print(makerHashes)
-
 # Maker ABI as per deployed code at Etherscan https://etherscan.io/address/0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2#contracts
 makerAbi = '''[{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"stop","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"guy","type":"address"},{"name":"wad","type":"uint256"}],"name":"approve","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"owner_","type":"address"}],"name":"setOwner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"src","type":"address"},{"name":"dst","type":"address"},{"name":"wad","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"guy","type":"address"},{"name":"wad","type":"uint256"}],"name":"mint","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"wad","type":"uint256"}],"name":"burn","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"name_","type":"bytes32"}],"name":"setName","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"src","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"stopped","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"authority_","type":"address"}],"name":"setAuthority","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"guy","type":"address"},{"name":"wad","type":"uint256"}],"name":"burn","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"wad","type":"uint256"}],"name":"mint","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"dst","type":"address"},{"name":"wad","type":"uint256"}],"name":"transfer","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"dst","type":"address"},{"name":"wad","type":"uint256"}],"name":"push","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"src","type":"address"},{"name":"dst","type":"address"},{"name":"wad","type":"uint256"}],"name":"move","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"start","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"authority","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"guy","type":"address"}],"name":"approve","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"src","type":"address"},{"name":"guy","type":"address"}],"name":"allowance","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"src","type":"address"},{"name":"wad","type":"uint256"}],"name":"pull","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[{"name":"symbol_","type":"bytes32"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"guy","type":"address"},{"indexed":false,"name":"wad","type":"uint256"}],"name":"Mint","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"guy","type":"address"},{"indexed":false,"name":"wad","type":"uint256"}],"name":"Burn","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"authority","type":"address"}],"name":"LogSetAuthority","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"owner","type":"address"}],"name":"LogSetOwner","type":"event"},{"anonymous":true,"inputs":[{"indexed":true,"name":"sig","type":"bytes4"},{"indexed":true,"name":"guy","type":"address"},{"indexed":true,"name":"foo","type":"bytes32"},{"indexed":true,"name":"bar","type":"bytes32"},{"indexed":false,"name":"wad","type":"uint256"},{"indexed":false,"name":"fax","type":"bytes"}],"name":"LogNote","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"owner","type":"address"},{"indexed":true,"name":"spender","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Approval","type":"event"}]'''
 
@@ -310,4 +272,43 @@ officialAbi = '''[
     }
 ]
 '''
+
+def getHashesForTesting(_abiData):
+    #ABI Text
+    abiData = re.sub(r"[\n\t]*", "", json.dumps(json.loads(_abiData), sort_keys=True))
+    abiData = re.sub(r"[\s]+", " ", abiData)
+    #Abi Sha
+    officialAbiSha = web3.toHex(web3.sha3(text=abiData))
+    # Abi JSON
+    officialAbiJSON = json.loads(abiData)
+    # Abi Functions
+    keccakHashes = []
+    for item in officialAbiJSON:
+        if item['type'] == 'function':
+            if len(item['inputs']) == 0:
+                stringToHash = str(item['name'] + '()')
+                hashCreated = str(web3.toHex(web3.sha3(text=stringToHash)))[2:10]
+                keccakHashes.append(hashCreated)
+            else:
+                tempString = ""
+                tempString += item['name'] + '('
+                iterator = 1
+                for functionInput in item['inputs']:
+                    if iterator == len(item['inputs']):
+                        tempString += str(functionInput['type'] + ')')
+                    else:
+                        tempString += str(functionInput['type'] + ',')
+                        iterator += 1
+                stringToHash = tempString
+                hashCreated = str(web3.toHex(web3.sha3(text=stringToHash)))[2:10]
+                keccakHashes.append(hashCreated)
+    return keccakHashes
+
+officialHashes = getHashesForTesting(officialAbi)
+makerHashes = getHashesForTesting(makerAbi)
+print("\nKeccak Hashes for official ERC20 ABI:")
+print(officialHashes)
+print("\nKeccak Hashes for Maker (MKR) ABI:")
+print(makerHashes)
+
 
