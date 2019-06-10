ad86d3ceab98192bbb32fae8fc42ca2346ff7c67
tpmccallum <mistermac2008@gmail.com>
2019-06-10 13:59:52 +1000

    Diagnose function call

--- python/utilities/manually_test_maker_abi_at_deployed_tx.py
+++ python/utilities/manually_test_maker_abi_at_deployed_tx.py
@@ -82,6 +82,6 @@ makerAbi = json.loads(abiData)
 txReceipt = web3.eth.getTransactionReceipt("0xcceb1fd34dcc4b18defa4ff29d51a225b20af8ed179db37da72ec5d5a4e8d385")
 tx = web3.eth.getTransaction("0xcceb1fd34dcc4b18defa4ff29d51a225b20af8ed179db37da72ec5d5a4e8d385")
 #makerInstance = web3.eth.contract(abi=makerAbi, address=txReceipt.contractAddress)
-batInstance = web3.eth.contract(abi=makerAbi, address="0x0d8775f648430679a709e98d2b0cb6250d2887ef")
+batInstance = web3.eth.contract(abi=makerAbi, address=web3.toChecksumAddress("0x0d8775f648430679a709e98d2b0cb6250d2887ef"))
 functionData = fetchPureViewFunctionData(batInstance)
 
