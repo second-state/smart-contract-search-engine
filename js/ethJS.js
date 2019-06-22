176b9e0cfe32918faf6957c0c2f11dabf48d19d7
tpmccallum <mistermac2008@gmail.com>
2019-06-22 18:42:28 +1000

    Define contract type by ABI hash

--- js/ethJS.js
+++ js/ethJS.js
@@ -529,6 +529,15 @@ function renderItems(_hits) {
         var lineBreak = jQuery("<hr/>", {});
         lineBreak.appendTo(".results");
 
+        var shaList = value.abiShalist;
+        if shaList.includes("0x2b5710e2cf7eb7c9bd50bfac8e89070bdfed6eb58f0c26915f034595e5443286"){
+            var type = jQuery("<dt/>", {
+                text: "Type: ERC20"
+            });
+            type.appendTo(dl);
+        }
+
+
         
 /*
         var description = jQuery("<dd/>", {
