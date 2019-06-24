ba1fc260661e61cf9058e9743e76e067c3ca5303
tpmccallum <mistermac2008@gmail.com>
2019-06-25 08:56:41 +1000

    search text

--- js/ethJS.js
+++ js/ethJS.js
@@ -264,9 +264,25 @@ $(document).ready(function() {
 $(document).ready(function() {
     $("#searchAddressButton").click(function() {
         $(".results").empty()
+        var theText = $("#searchTextInput").val();
+        if($.trim(theText.length) > "0"){
+            iQuery = {};
+            iQuery["query"] = theText;
+            iQueryString = {};
+            iQueryString["query_string"] = iQuery;
+            oQuery = {};
+            oQuery["query"] = iQueryString;
+            jsonString = JSON.stringify(oQuery);
+            getItemsUsingDataViaFlask(jsonString);
+        }
+        else{
         getQuickItemsViaFlask(elasticSearchUrl);
+        }
     });
 });
+
+
+
 
 $(document).ready(function() {
     $("#indexContractButton").click(function() {
