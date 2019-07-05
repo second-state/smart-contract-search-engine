e0d704fae02fbfe576d007f32871acf3a28246ad
tpmccallum <mistermac2008@gmail.com>
2019-07-05 16:12:00 +1000

    Return the abiSha3 (from io.py to JS) as json

--- python/io.py
+++ python/io.py
@@ -10,7 +10,6 @@ harvester = Harvest()
 
 app = Flask(__name__)
 
-
 @app.route("/api/submit_abi", methods=['GET', 'POST'])
 def submit_abi():
     jsonRequestData = json.loads(request.data)
@@ -40,31 +39,16 @@ def submit_abi():
         doc["response"] = 'Successfully indexed contract.'
         return jsonify(doc)
 
-# START - New experimental endpoints for ES-SS.JS work
-######################################################
 @app.route("/api/sha_an_abi", methods=['GET', 'POST'])
 def sha_an_abi():
     print(request)
     jsonRequestData = json.loads(request.data)
-    abi = jsonRequestData["abi"]
+    abi = json.loads(jsonRequestData["abi"])
     abiHash = harvester.shaAnAbi(abi)
     result = {}
     result["abiSha3"] = abiHash
     print(result)
     return jsonify(result)
-
-@app.route("/api/process_single_transaction", methods=['GET', 'POST'])
-def process_single_transaction():
-    print(request)
-    jsonRequestData = json.loads(request.data)
-    abi = json.loads(jsonRequestData["abi"])
-    txHash = str(jsonRequestData["tx"])
-    results = harvester.processSingleTransaction(abi, txHash)
-    print(results)
-    #return jsonify(results["results"])
-
-# END - New experimental endpoints for ES-SS.JS work
-####################################################
 
 @app.route("/api/es_quick_100_search", methods=['GET', 'POST'])
 def es_quick_100_search():
