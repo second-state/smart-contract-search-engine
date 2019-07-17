fd201bbd9b3476718fd3a124eb07d75eb7f2c941
tpmccallum <mistermac2008@gmail.com>
2019-07-18 09:12:52 +1000

    Adding one generic  ABI so that system does not error when initially started

--- python/harvest.py
+++ python/harvest.py
@@ -739,6 +739,17 @@ if __name__ == "__main__":
     # Ensure that the ABI index exists (others are created programatically but ABI needs to exist up front)
     if harvester.es.indices.exists(index=harvester.abiIndex) == False:
         harvester.es.indices.create(index=harvester.abiIndex)
+        # Upload a single ABI so that the system does not error when started (requires at least one abi to function)
+        abiUrl1 = "https://raw.githubusercontent.com/tpmccallum/test_endpoint2/master/erc20_transfer_function_only_abi.txt"
+        abiData1 = requests.get(abiUrl1).content
+        abiData1JSON = json.loads(abiData1)
+        theDeterministicHash1 = harvester.shaAnAbi(abiData1JSON)
+        cleanedAndOrderedAbiText1 = harvester.cleanAndConvertAbiToText(abiData1JSON)
+        data1 = {}
+        data1['indexInProgress'] = "false"
+        data1['epochOfLastUpdate'] = int(time.time())
+        data1['abi'] = cleanedAndOrderedAbiText1
+        harvester.es.index(index=harvester.abiIndex, id=theDeterministicHash1, body=data1)
     else:
         print("Index already exists")
     if args.mode == "full":
