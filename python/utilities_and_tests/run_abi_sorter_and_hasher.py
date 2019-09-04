c885eebd79e217ec9a701ee7f0f63f83441814e3
tpmccallum <mistermac2008@gmail.com>
2019-09-04 13:23:26 +1000

    Adding new test cases

--- python/utilities_and_tests/run_abi_sorter_and_hasher.py
+++ python/utilities_and_tests/run_abi_sorter_and_hasher.py
@@ -8,7 +8,7 @@ harvester = Harvest()
 abiUrls = []
 abiUrls.append("https://raw.githubusercontent.com/tpmccallum/mixed_ordered_erc20_abis_for_testing/master/vanilla.txt")
 abiUrls.append("https://raw.githubusercontent.com/tpmccallum/test_endpoint2/master/type_and_name_reversed.txt.txt")
-abiUrls.append("https://raw.githubusercontent.com/tpmccallum/mixed_ordered_erc20_abis_for_testing/master/anonymous_and_inputs_reversed.txt")
+#abiUrls.append("https://raw.githubusercontent.com/tpmccallum/mixed_ordered_erc20_abis_for_testing/master/anonymous_and_inputs_reversed.txt")
 
 
 outputHashes = []
