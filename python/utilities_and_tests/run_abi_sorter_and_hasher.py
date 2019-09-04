d4f35bca8d8bb8716c75cd11745c291b393e95d7
tpmccallum <mistermac2008@gmail.com>
2019-09-04 13:12:22 +1000

    Adding new test cases

--- python/utilities_and_tests/run_abi_sorter_and_hasher.py
+++ python/utilities_and_tests/run_abi_sorter_and_hasher.py
@@ -6,12 +6,13 @@ harvester = Harvest()
 
 # RAW text of ABIs for sorting and hashing
 abiUrls = []
-abiUrls.append("https://raw.githubusercontent.com/tpmccallum/test_endpoint2/master/vanilla.txt")
+abiUrls.append("https://raw.githubusercontent.com/tpmccallum/mixed_ordered_erc20_abis_for_testing/master/vanilla.txt")
 abiUrls.append("https://raw.githubusercontent.com/tpmccallum/mixed_ordered_erc20_abis_for_testing/master/extra_spaces.txt")
 abiUrls.append("https://raw.githubusercontent.com/tpmccallum/test_endpoint2/master/type_and_name_reversed.txt.txt")
 
 outputHashes = []
 for singleAbiUrl in abiUrls:
+    print("Processing: " + singleAbiUrl)
     singleAbiString = requests.get(singleAbiUrl).content
     singleAbiJSON = json.loads(singleAbiString)
     singleHash = harvester.shaAnAbi(singleAbiJSON)
