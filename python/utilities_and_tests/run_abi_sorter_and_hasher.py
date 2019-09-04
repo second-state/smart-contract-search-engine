413498c03d86cd35bcff6248f14106308bc73174
tpmccallum <mistermac2008@gmail.com>
2019-09-04 13:10:35 +1000

    Adding new test cases

--- python/utilities_and_tests/run_abi_sorter_and_hasher.py
+++ python/utilities_and_tests/run_abi_sorter_and_hasher.py
@@ -7,7 +7,7 @@ harvester = Harvest()
 # RAW text of ABIs for sorting and hashing
 abiUrls = []
 abiUrls.append("https://raw.githubusercontent.com/tpmccallum/test_endpoint2/master/vanilla.txt")
-abiUrls.append("https://raw.githubusercontent.com/tpmccallum/test_endpoint2/master/extra_spaces.txt")
+abiUrls.append("https://raw.githubusercontent.com/tpmccallum/mixed_ordered_erc20_abis_for_testing/master/extra_spaces.txt")
 abiUrls.append("https://raw.githubusercontent.com/tpmccallum/test_endpoint2/master/type_and_name_reversed.txt.txt")
 
 outputHashes = []
