a463c6b84b41c187c5b1b7cda762424ffa5cc1a4
tpmccallum <mistermac2008@gmail.com>
2019-09-05 15:38:00 +1000

    Output

--- python/utilities_and_tests/create_random_abi_then_test.py
+++ python/utilities_and_tests/create_random_abi_then_test.py
@@ -89,6 +89,7 @@ singleAbiJSON = json.loads(singleAbiString)
 for i in range(20):
     string = randomizeAndConvertAbiToText(singleAbiJSON)
     randomAbis.append(string)
+print(str(len(randomAbis)))
 
 print("Hashes of random ABIs")
 for rItem in randomAbis:
