6a2bd0d03fa24c237b0c632d8f6d658267e1b02c
tpmccallum <mistermac2008@gmail.com>
2019-09-17 10:58:48 +1000

    Harvesting event logs via the block iteration method which the current full and topup modes utilizes

--- python/utilities_and_tests/event_log_harvest_testing.py
+++ python/utilities_and_tests/event_log_harvest_testing.py
@@ -5,7 +5,23 @@ import requests
 from harvest import Harvest
 
 harvester = Harvest()
-
-event_filter = harvester.web3.eth.filter('latest')
-for event in event_filter.get_new_entries():
-	print(event)
+latestBlockNumber = harvester.web3.eth.getBlock('latest').number
+for b in range(latestBlockNumber - 1, latestBlockNumber):
+    print("\n")
+    transactionCount = harvester.web3.eth.getBlockTransactionCount(b)
+    if(transactionCount >= 1):
+        print("Transaction count is " + str(transactionCount))
+        for singleTransactionInt in range(0, transactionCount):
+            print("\n")
+            print("Processing transaction " + str(singleTransactionInt)+ " of block " + str(b))
+            transaction = harvester.web3.eth.getTransactionByBlock(b, singleTransactionInt)
+            transactionHash = transaction.hash
+            transactionReceipt = harvester.web3.eth.getTransactionReceipt(transaction.hash)
+            transactionLogs = transactionReceipt.logs
+            if (len(transactionLogs) >= 1):
+                print("Block " + str(b) + "\'s transaction log[s] are below.")
+                print(transactionLogs)
+            else:
+                print("Block " + str(b) + "has no logs")
+    else:
+        print("Transaction count is 0")
